3ab6aa2cc5afab74df7263f8498639fed1b183a9
Taylor Ellis <Taylorellis11@gmail.com>
2018-04-06 16:53:51 -0700

    Problem 5 passing.

--- app.js
+++ app.js
@@ -96,12 +96,15 @@ IMPORTANT DETAIL: You may not use the arithmetic operator * in this function. To
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testMultiplyArray() function and see if the test passes.*/
 
 // Write your code here
-function multiplyArray(multArr) { //eslint-disable-line
-
-}
+function multiplyArray() {
+  var a = testArray[0];
+  var b = testArray[1];
+  var c = testArray[2];
+  return([(multiply(a, multiply(b, c)[0])[0]), 'The numbers ' + a + ',' + b + ',' + c + ' have a product of ' + (multiply(a, multiply(b, c)[0])[0]) + '.']);
+  }
 
 // Here is the test for multiplyArray(); uncomment it to run it
-// testMultiplyArray(testArray);
+testMultiplyArray(testArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop.
 
